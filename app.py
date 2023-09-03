205ec791d32e9f2826705b2404e9e6b79c1f7d74
AI_Programming <118508502+IA-Programming@users.noreply.github.com>
2023-09-03 03:43:48 +0000

    first try to gave and ui for

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
 # First
 import openai 
 import streamlit as st
+import openai
 import interpreter
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-
-st.title("💬 Open Interpreter")
+if 'OpenAI_api_key' not in st.session_state:
+    st.session_state.OpenAI_api_key = ''
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+st.set_page_config(
+   page_title="Open-Interpreter Gpt App",
+   page_icon="🤖",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
+st.title("💬 Open Interpreter")
+
+with st.sidebar:
+    def submit():
+        try:
+            st.session_state.OpenAI_api_key = st.session_state.widget
+            openai.api_key = st.session_state.OpenAI_api_key
+            respuesta = openai.ChatCompletion.create(
+            model="gpt-4-0613",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Hello!"}
+            ]
+            )
+            interpreter.api_key = st.session_state.OpenAI_api_key
+        except Exception as e:
+            print(e)
+            # st.session_state.OpenAI_api_key = st.session_state.widget
+            st.session_state.widget = ''
+            # st.session_state.OpenAI_api_key = ''
+            st.info("Please add your OpenAI API key Correctly to continue.")
+            # st.stop()
+
+    st.text_input('OpenAI API Key', key='widget', on_change=submit, type="password")
+
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+if prompt := st.chat_input(placeholder="Write here your message", disabled=not st.session_state.OpenAI_api_key):
 
-    openai.api_key = openai_api_key
+    openai.api_key = st.session_state.OpenAI_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    response = interpreter.chat(prompt)
+    st.session_state.messages.append(response)
+    st.chat_message("assistant").write(response)
