6de18b69e040c3c4a70e0e95bef0f5eb9add5a4e
AI_Programming <118508502+IA-Programming@users.noreply.github.com>
2023-10-03 08:35:21 +0000

    0.1 Version Usable

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # GuiStreamlitOpenInterpreter
-Trying to make a gui to use Open Interpreter
+Trying to make an easy gui to use Open Interpreter
+
+![](UI.png)
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+GuiStreamlitOpenInterpreter is a project aimed at simplifying the process of running code and interacting with OpenAI models through a graphical user interface (GUI). With Streamlit as the frontend framework, this tool provides an intuitive way to work with Python and OpenAI applications without needing to write code in a traditional coding environment.
+
+## Features
+
+- User-friendly interface for running Python code and others.
+- Integration with OpenAI applications for natural language processing and chatbot functionalities.
+- Simplified execution of code and interaction with OpenAI models.
+- Customizable and extensible for different use cases.
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- [Python](https://www.python.org/) (>= 3.10) installed on your system.
+- Dependencies mentioned in the `requirements.txt` file.
+
+### Installation
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/IA-Programming/GuiStreamlitOpenInterpreter.git
+   ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd GuiStreamlitOpenInterpreter
+    ```
+
+3. Install the required dependencies using pip:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+1. To run the GuiStreamlitOpenInterpreter, use the following command:
+    ```bash
+    streamlit run streamlit_app.py --server.port 8501
+    ```
+2. Access the application in your web browser at http://localhost:8501.
+3. Use the GUI to execute Python code and interact with OpenAI applications.
+
+## Contributing
+
+Contributions are welcome! If you'd like to contribute to this project, please follow these steps:
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them with a clear commit message.
+4. Push your changes to your fork.
+5. Create a pull request to merge your changes into the main repository.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # First
-import openai 
+from streamlit.components.v1 import html
+from streamlit_extras.add_vertical_space import add_vertical_space
+import openai
+# import json
 import streamlit as st
 import openai
 import interpreter
@@ -7,7 +10,8 @@ import interpreter
 if 'OpenAI_api_key' not in st.session_state:
     st.session_state.OpenAI_api_key = ''
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = []
+    # [{"role": "assistant", "content": "How can I help you?"}]
 
 st.set_page_config(
    page_title="Open-Interpreter Gpt App",
@@ -24,32 +28,91 @@ with st.sidebar:
             st.session_state.OpenAI_api_key = st.session_state.widget
             openai.api_key = st.session_state.OpenAI_api_key
             respuesta = openai.ChatCompletion.create(
-            model="gpt-4-0613",
+            model="gpt-3.5-turbo-0613",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": "Hello!"}
             ]
             )
             interpreter.api_key = st.session_state.OpenAI_api_key
+            interpreter.auto_run = True
         except Exception as e:
             print(e)
-            # st.session_state.OpenAI_api_key = st.session_state.widget
             st.session_state.widget = ''
-            # st.session_state.OpenAI_api_key = ''
+            st.session_state.OpenAI_api_key = ''
             st.info("Please add your OpenAI API key Correctly to continue.")
-            # st.stop()
 
     st.text_input('OpenAI API Key', key='widget', on_change=submit, type="password")
 
+    add_vertical_space(29)
+    html_chat = '<center><h5>🤗 Soporta el proyecto con una donación para el desarrollo de nuevas Características 🤗</h5>'
+    st.markdown(html_chat, unsafe_allow_html=True)
+    button = '<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="blazzmocompany" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>'
+    html(button, height=70, width=220)
+    iframe = '<style>iframe[width="220"]{position: absolute; top: 50%;left: 50%;transform: translate(-50%, -50%);margin:26px 0}</style>'
+    st.markdown(iframe, unsafe_allow_html=True)
+    add_vertical_space(2)
+    st.write('<center><h6>Hecho con ❤️ por <a href="mailto:blazzmo.company@gmail.com">AI-Programming</a></h6>',unsafe_allow_html=True)
 
+# st.write(st.session_state)
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    if msg["role"]=="user":
+        st.chat_message(msg["role"]).text(msg["content"])
+    elif msg["role"]=="assistant":
+        st.chat_message(msg["role"]).markdown(msg["content"])
 
 if prompt := st.chat_input(placeholder="Write here your message", disabled=not st.session_state.OpenAI_api_key):
+    st.write(interpreter.messages)
 
-    openai.api_key = st.session_state.OpenAI_api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = interpreter.chat(prompt)
-    st.session_state.messages.append(response)
-    st.chat_message("assistant").write(response)
+    interpreter.model = "gpt-3.5-turbo"
+    
+    with st.chat_message("user"):
+        st.text(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        # Initialize variables
+        full_response = ""
+        message_placeholder = st.empty()
+        for chunk in interpreter.chat(prompt, display=False, stream=True):
+            
+            # Message
+            if "message" in chunk:
+                full_response += chunk["message"]
+                if chunk['message'] == ":":
+                    full_response += "\n"
+
+            # Code
+            if "code" in chunk:
+              # Handle code lines
+              if full_response.endswith("```"):
+                full_response = full_response[:len(full_response)-3] + chunk['code'] + "```"
+              else:
+                full_response += f"```{chunk['code']}```"
+            
+            # Output
+            if "executing" in chunk:
+                # Handle code execution messages
+                if full_response.endswith("```"):
+                    full_response = full_response[:len(full_response)-3] + "\n```"
+                full_response += f"\n\n```{chunk['executing']['language']}\n{chunk['executing']['code']}\n```"
+            if "output" in chunk:
+                # Handle output messages
+                if chunk["output"] != "KeyboardInterrupt":
+                    full_response += f"\n\n```text\n{chunk['output']}```\n"
+            if "end_of_execution" in chunk:
+                # Add a newline to separate executions
+                full_response = full_response.strip()
+                full_response += "\n"
+
+            # Join the formatted messages
+            # full_response += json.dumps(chunk)
+            message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+elif not st.session_state.OpenAI_api_key:
+    st.info("👋 Hey , estamos muy felices por verte aqui 🤗")
+    st.info("👉 Coloca tu OpenAI api key, para ser capaz to correr codigo mientras lo generas 🚀")
+    st.error("👉 El objetivo de este proyecto es tu mostrar una facil implementacion del uso de Open Interpreter 🤗")
